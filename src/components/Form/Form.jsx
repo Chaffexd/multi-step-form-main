c7f6a6af92b26af4f80cc4f87bf7a8a4d2c7fd41
chaffexd <shanechaffe@gmail.com>
2023-03-27 13:34:06 +0100

    commit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,6 +5,7 @@ import Steps from '../Steps/Steps';
 import classes from './Form.module.css';
 
 const Form = () => {
+  // Name section
   const {
     value: enteredName,
     hasError: nameError,
@@ -14,9 +15,28 @@ const Form = () => {
     resetInput: resetName
   } = useInput((value) => value.trim() !== "");
 
+  // Email section
+  const {
+    value: enteredEmail,
+    hasError: emailError,
+    isValid: emailIsValid, 
+    valueChangedHandler: emailChangedHandler,
+    inputBlurHandler: emailBlur,
+    resetInput: resetEmail
+  } = useInput((value) => value.trim() !== "" && value.includes("@") && value.length > 4);
+
+  const {
+    value: enteredPhoneNumber,
+    hasError: phoneNumberError,
+    isValid: phoneNumberIsValid, 
+    valueChangedHandler: phoneNumberChangedHandler,
+    inputBlurHandler: phoneNumberBlur,
+    resetInput: resetPhoneNumber
+  } = useInput((value) => value.trim() !== "");
+
   let isFormValid = false;
 
-  if(nameIsValid) {
+  if(nameIsValid && emailIsValid && phoneNumberIsValid) {
     isFormValid = true;
   }
 
@@ -24,13 +44,20 @@ const Form = () => {
     e.preventDefault();
 
     // if entered name is not present, just return and stop
-    if(!enteredName) {
+    if(!enteredName && !emailIsValid && !phoneNumberIsValid) {
       return;
     };
+    console.log(enteredName, enteredEmail, enteredPhoneNumber)
 
     // reset fields
     resetName("");
+    resetEmail("");
+    resetPhoneNumber("");
   };
+
+  const nameInputClasses = nameError ? `${classes.formInput} ${classes.invalid}` : `${classes.formInput}`;
+  const emailInputClasses = emailError ? `${classes.formInput} ${classes.invalid}` : `${classes.formInput}`;
+  const phoneNumberInputClasses = phoneNumberError ? `${classes.formInput} ${classes.invalid}` : `${classes.formInput}`;
 
   return (
     <div className={classes.formContainer}>
@@ -39,7 +66,7 @@ const Form = () => {
           <div>
             <h1>Personal Info</h1>
             <p>Please provide your name, email address and phone number.</p>
-            <div className={classes.formInput}>
+            <div className={nameInputClasses}>
               <label htmlFor="name" >Name</label>
               <input 
                 type="text" 
@@ -51,13 +78,29 @@ const Form = () => {
               />
               {nameError && <span className={classes.errorText}>Please enter a name.</span>}
             </div>
-            <div className={classes.formInput}>
+            <div className={emailInputClasses}>
               <label htmlFor="email" >Email</label>
-              <input type="email" id="email" placeholder="e.g. stephenking@lorem.com" />
+              <input 
+                type="email" 
+                id="email" 
+                placeholder="e.g. stephenking@lorem.com"
+                value={enteredEmail}
+                onChange={emailChangedHandler}
+                onBlur={emailBlur}
+              />
+              {emailError && <span className={classes.errorText}>Please enter a valid email.</span>}
             </div>
-            <div className={classes.formInput}>
+            <div className={phoneNumberInputClasses}>
               <label htmlFor="mobile" >Phone Number</label>
-              <input type="text" id="mobile" placeholder="e.g. +1 234 567 890" />
+              <input 
+                type="text" 
+                id="mobile" 
+                placeholder="e.g. +1 234 567 890"
+                value={enteredPhoneNumber}
+                onChange={phoneNumberChangedHandler}
+                onBlur={phoneNumberBlur}
+              />
+              {phoneNumberError && <span className={classes.errorText}>Please enter a valid phone number.</span>}
             </div>
           </div>
           <button disabled={!isFormValid} >Next Step</button>
